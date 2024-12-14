f5c931bf02767ad3e225387b8d8acaa8295318fe
Douglas Ronne <vextorspace@gmail.com>
2024-12-14 19:57:04 -0400

    Assistant: - Added TestRunner mock for run_tests - Created new Assistant instance

--- src/assistant.rs
+++ src/assistant.rs
@@ -9,8 +9,19 @@ impl Assistant {
 }
 #[cfg(test)]
 mod tests {
+    use mockall::mock;
+    use super::*;
+    use std::path::PathBuf;
+    use crate::test_runner::test_results::TestResults;
+
     #[test]
     fn instatiates() {
+        mock!{
+            pub TestRunner {
+                fn run_tests(&self, path: PathBuf) -> TestResults;
+            }
+        }
         let _ = super::Assistant::new();
+
     }
 }
