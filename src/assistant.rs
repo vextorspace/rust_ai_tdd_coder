edd76425b39efbdab64b3ab759a829d7a7488e4d
Douglas Ronne <vextorspace@gmail.com>
2024-12-14 20:19:53 -0400

    Assistant: - Add automock for TestProvider - Implement run_tests in TestProvider

--- src/assistant.rs
+++ src/assistant.rs
@@ -15,18 +15,18 @@ impl Assistant {
 }
 #[cfg(test)]
 mod tests {
-    use mockall::mock;
+    use mockall::{automock, mock};
     use super::*;
     use std::path::PathBuf;
     use crate::test_runner::test_results::TestResults;
 
+    #[automock]
+    pub trait TestProvider {
+        fn run_tests(&self, path: PathBuf) -> TestResults;
+    }
+
     #[test]
     fn instatiates() {
-        mock!{
-            pub TestProvider {
-                fn run_tests(&self, path: PathBuf) -> TestResults;
-            }
-        }
         let mut mock_test_provider = MockTestProvider::new();
         mock_test_provider.expect_run_tests().times(0);
 
