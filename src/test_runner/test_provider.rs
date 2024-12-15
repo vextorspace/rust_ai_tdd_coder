0fe884fb7136403653977741641e4ee27e93fb84
Douglas Ronne <vextorspace@gmail.com>
2024-12-14 20:15:10 -0400

    Assistant: Remove pub(crate) visibility modifier from TestProvider trait

--- src/test_runner/test_provider.rs
+++ src/test_runner/test_provider.rs
@@ -1,6 +1,6 @@
 use crate::test_runner::test_results::TestResults;
 use std::path::PathBuf;
 
-pub(crate) trait TestProvider {
+pub trait TestProvider {
     fn run_tests(&self, path: PathBuf) -> TestResults;
 }
