2a317e6b9a7e9dbcca228ddad235d8077f22ce38
Douglas Ronne <vextorspace@gmail.com>
2024-12-15 12:06:30 -0800

    Assistant: Refactor Assistant struct and constructor

--- src/assistant.rs
+++ src/assistant.rs
@@ -3,54 +3,21 @@ use crate::git::version_control::VersionControl;
 use crate::ai::ai_coder::AiCoder;
 
 pub struct Assistant {
-    test_provider: Option<Box<dyn TestProvider>>,
-    version_control: Option<Box<dyn VersionControl>>,
-    ai_coder: Option<Box<dyn AiCoder>>,
+    test_provider: Box<dyn TestProvider>,
+    version_control: Box<dyn VersionControl>,
+    ai_coder: Box<dyn AiCoder>,
 }
 
 impl Assistant {
-    pub fn new() -> Assistant {
+    pub fn new(
+        test_provider: Box<dyn TestProvider>,
+        version_control: Box<dyn VersionControl>,
+        ai_coder: Box<dyn AiCoder>,
+    ) -> Assistant {
         Assistant {
-            test_provider: None,
-            version_control: None,
-            ai_coder: None,
-        }
-    }
-
-    pub fn builder() -> AssistantBuilder {
-        AssistantBuilder::new()
-    }
-    pub fn with_test_provider(&mut self, test_provider: Box<dyn TestProvider>) -> &mut Self {
-        self.test_provider = Some(test_provider);
-        self
-    }
-
-    pub fn with_version_control(&mut self, version_control: Box<dyn VersionControl>) -> &mut Self {
-        self.version_control = Some(version_control);
-        self
-    }
-
-    pub fn with_ai_coder(&mut self, ai_coder: Box<dyn AiCoder>) -> &mut Self {
-        self.ai_coder = Some(ai_coder);
-        self
-    }
-}
-
-pub struct AssistantBuilder {
-}
-
-impl AssistantBuilder {
-    fn new() -> AssistantBuilder {
-        AssistantBuilder {
-        }
-    }
-
-
-    pub fn build(&self) -> Assistant {
-        Assistant {
-            test_provider: None,
-            version_control: None,
-            ai_coder: None,
+            test_provider: test_provider,
+            version_control: version_control,
+            ai_coder: ai_coder,
         }
     }
 }
@@ -63,10 +30,12 @@ mod tests {
     use crate::ai::ai_coder::MockAiCoder;
 
     #[test]
-    fn builder_creates_empty() {
-        let assistant = Assistant::builder().build();
-        assert!(assistant.test_provider.is_none());
-        assert!(assistant.version_control.is_none());
-        assert!(assistant.ai_coder.is_none());
+    fn instantiates() {
+        let test_provider = Box::new(MockTestProvider::new());
+        let version_control = Box::new(MockVersionControl::new());
+        let ai_coder = Box::new(MockAiCoder::new());
+        let _assistant = Assistant::new(test_provider, version_control, ai_coder);
+
+
     }
 }
