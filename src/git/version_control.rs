9f2416df35b21a25ffd6d69dbe4a163a9ac8f005
Douglas Ronne <vextorspace@gmail.com>
2024-12-14 20:32:13 -0400

    Assistant: - Add mockall automock attribute - Implement automock for VersionControl trait

--- src/git/version_control.rs
+++ src/git/version_control.rs
@@ -1,5 +1,7 @@
 use std::path::PathBuf;
+use mockall::automock;
 
+#[cfg_attr(test, automock)]
 pub trait VersionControl {
     fn commit(&self, path: PathBuf);
     fn reject(&self, path: PathBuf);
