a94b49c7a7a7fb2566eec5bcacb1ad268fb3b1f8
Douglas Ronne <vextorspace@gmail.com>
2024-12-14 20:27:07 -0400

    Assistant: - Added cfg_attr test to trait

--- src/test_runner/test_provider.rs
+++ src/test_runner/test_provider.rs
@@ -2,7 +2,7 @@ use crate::test_runner::test_results::TestResults;
 use std::path::PathBuf;
 use mockall::automock;
 
-#[automock]
+#[cfg_attr(test, automock)]
 pub trait TestProvider {
     fn run_tests(&self, path: PathBuf) -> TestResults;
 }
