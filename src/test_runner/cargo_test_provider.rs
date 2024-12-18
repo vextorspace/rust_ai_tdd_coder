7c1d85abb6345ecc54baded17c5fc4d2fa32b55e
Douglas Ronne <vextorspace@gmail.com>
2024-12-18 07:17:24 -0800

    Assistant: Added test command creation method.

--- src/test_runner/cargo_test_provider.rs
+++ src/test_runner/cargo_test_provider.rs
@@ -1,12 +1,19 @@
 use super::test_provider::TestProvider;
 use super::test_results::TestResults;
 use std::path::PathBuf;
+use std::process::Command;
 
 pub struct CargoTestProvider {}
 
 impl CargoTestProvider {
     pub fn new() -> CargoTestProvider {
         CargoTestProvider {}
+    }
+
+    fn make_test_command(&self, path: &PathBuf) -> Command {
+        let mut command = Command::new(path);
+        command.arg("test");
+        command
     }
 }
 
@@ -20,10 +27,20 @@ impl TestProvider for CargoTestProvider {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::process::CommandArgs;
 
     #[test]
     fn instantiates() {
         let _provider: Box<dyn TestProvider> = Box::new(CargoTestProvider::new());
+    }
+
+    #[test]
+    fn creates_command() {
+        let provider = CargoTestProvider::new();
+        let command = provider.make_test_command(&PathBuf::from("/tests"));
+        let mut args = command.get_args();
+        let test_argument = args.next();
+        assert!(test_argument.is_some());
     }
 
 
