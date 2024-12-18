6538887fb3b636f55b3266d42293a690f8631e78
Douglas Ronne <vextorspace@gmail.com>
2024-12-18 10:38:31 -0800

    Assistant: Refactor main function to use helper functions.

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,17 @@ use rust_ai_tdd_coder::git::git_version_control::GitVersionControl;
 use rust_ai_tdd_coder::test_runner::cargo_test_provider::CargoTestProvider;
 
 fn main() -> Result<()>{
-    let lang = var("TARGET_LANGUAGE")?.to_lowercase();
-    let test_provider = match lang.as_str() {
-        "rust" => {
-            Ok(Box::new(CargoTestProvider::new()))
-        },
-        _ => {
-            Err(anyhow!("Unsupported language: {}", lang))
-        }
-    }?;
+    let test_provider = make_test_provider()?;
 
+    let version_controller = make_version_control()?;
+
+    let commit_generator = Box::new(ConstantCommitMessage::new("Working".to_string()));
+    let assistant = rust_ai_tdd_coder::assistant::Assistant::new(test_provider, version_controller,commit_generator);
+
+    Ok(())
+}
+
+fn make_version_control() -> Result<Box<GitVersionControl>> {
     let vcs = var("VERSION_CONTROL")?.to_lowercase();
     let version_controller = match vcs.as_str() {
         "git" => {
@@ -24,9 +25,18 @@ fn main() -> Result<()>{
             Err(anyhow!("Unsupported version control system: {}", vcs))
         }
     }?;
+    Ok(version_controller)
+}
 
-    let commit_generator = Box::new(ConstantCommitMessage::new("Working".to_string()));
-    let assistant = rust_ai_tdd_coder::assistant::Assistant::new(test_provider, version_controller,commit_generator);
-
-    Ok(())
+fn make_test_provider() -> Result<Box<CargoTestProvider>> {
+    let lang = var("TARGET_LANGUAGE")?.to_lowercase();
+    let test_provider = match lang.as_str() {
+        "rust" => {
+            Ok(Box::new(CargoTestProvider::new()))
+        },
+        _ => {
+            Err(anyhow!("Unsupported language: {}", lang))
+        }
+    }?;
+    Ok(test_provider)
 }
