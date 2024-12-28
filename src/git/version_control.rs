0425e957bd8b361c110b71def2593885a8313c63
Douglas Ronne <vextorspace@gmail.com>
2024-12-28 13:44:21 -0400

    Working

--- src/git/version_control.rs
+++ src/git/version_control.rs
@@ -1,10 +1,36 @@
 use std::path::PathBuf;
+use std::any::Any;
 use mockall::automock;
 use anyhow::Result;
+use downcast_rs::{impl_downcast, Downcast};
+use crate::git::git_version_control::GitVersionControl;
 
 #[cfg_attr(test, automock)]
-pub trait VersionControl {
+pub trait VersionControl: Downcast {
     fn commit(&self, path: &PathBuf, message: String) -> Result<()>;
     fn reject(&self, path: &PathBuf) -> Result<()>;
     fn diff(&self, path: &PathBuf) -> Result<String>;
+}
+
+impl_downcast!(VersionControl);
+
+pub struct VersionControlBuilder ();
+
+impl VersionControlBuilder {
+    pub fn default() -> Box<dyn VersionControl> {
+        Box::new(GitVersionControl::new())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    
+    #[test]
+    fn builder_default_makes_git() {
+        let version_control = VersionControlBuilder::default();
+
+        assert!(version_control.as_ref().downcast_ref::<GitVersionControl>().is_some());
+    }
 }
