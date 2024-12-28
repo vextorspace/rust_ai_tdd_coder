ea9fea1042f3c29f342f3dc9f19beaa7b23be4a7
Douglas Ronne <vextorspace@gmail.com>
2024-12-28 11:24:54 -0400

    Working

--- src/git/git_version_control.rs
+++ src/git/git_version_control.rs
@@ -43,9 +43,17 @@ impl VersionControl for GitVersionControl {
     fn commit(&self, path: &PathBuf, message: String) -> Result<()>{
         println!("Accepting Changes");
         let mut add_command = self.make_add_command(path);
-        add_command.status()?;
+        let add_status = add_command.status()?;
+        if !add_status.success() {
+            return Err(anyhow::anyhow!("Failed to add files"));
+        }
+        println!("Added files");
         let mut command = self.make_commit_command(path, message);
-        command.status()?;
+        let commit_status = command.status()?;
+        if !commit_status.success() {
+            return Err(anyhow::anyhow!("Failed to commit files"));
+        }
+        println!("Committed files");
         Ok(())
     }
 
