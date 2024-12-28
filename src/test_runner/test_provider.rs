2cabb4d076691b85b18a943ec97c21f40e92b6fc
Douglas Ronne <vextorspace@gmail.com>
2024-12-28 14:15:44 -0400

    Working...

--- src/test_runner/test_provider.rs
+++ src/test_runner/test_provider.rs
@@ -1,5 +1,6 @@
 use crate::test_runner::test_results::TestResults;
 use std::path::PathBuf;
+use dotenv::var;
 use downcast_rs::{impl_downcast, Downcast};
 use mockall::automock;
 
@@ -13,6 +14,17 @@ impl_downcast!(TestProvider);
 pub struct TestProviderFactory {}
 
 impl TestProviderFactory {
+    pub(crate) fn default() -> Box<dyn TestProvider> {
+        let lang_result = var("TARGET_LANGUAGE");
+        let l = if let Ok(lang) = lang_result {
+            lang.to_string()
+        } else {
+            "rust".to_string()
+        };
+
+        TestProviderFactory::from_lang(l.as_str())
+    }
+
     pub fn from_lang(lang: &str) -> Box<dyn TestProvider> {
         match lang {
             "rust" => Box::new(crate::test_runner::cargo_test_provider::CargoTestProvider::new()),
@@ -30,6 +42,12 @@ mod tests {
     fn from_lang_makes_cargo_for_rust() {
         let provider = TestProviderFactory::from_lang("rust");
 
+        assert!(provider.is::<CargoTestProvider>());
+    }
+
+    #[test]
+    fn default_looks_for_env_lang() {
+        let provider = TestProviderFactory::default();
         assert!(provider.is::<CargoTestProvider>());
     }
 }
