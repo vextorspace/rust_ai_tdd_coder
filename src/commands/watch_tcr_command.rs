9d98e04d7644dd805fcea6d878f49b2e45fa6849
Douglas Ronne <vextorspace@gmail.com>
2025-01-08 09:29:20 -0800

    Removed no-change execution test for assistant.

--- src/commands/watch_tcr_command.rs
+++ src/commands/watch_tcr_command.rs
@@ -31,29 +31,4 @@ mod tests {
 
         assert_eq!(command.as_ref().get_label(), "watch_tcr");
     }
-
-    #[tokio::test]
-    async fn execute_does_not_call_assistant_on_no_change() {
-        let mut assistant = MockAssistant::new();
-        assistant.expect_tcr().times(0);
-        let command = WatchCommand::new();
-
-
-
-        use tokio::time::{sleep, Duration};
-        use std::time::Instant;
-
-        let handle = tokio::task::spawn(async move {
-            command.execute(Box::new(&assistant), PathBuf::from("bob")).expect("should not fail");
-        });
-
-
-        tokio::task::spawn(async move {
-            let start_time = Instant::now();
-            sleep(Duration::from_secs(1)).await;
-
-            handle.abort();
-        });
-    }
-
 }
