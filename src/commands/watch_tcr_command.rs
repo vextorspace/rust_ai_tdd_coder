bdd41e81ad8ad448acd7b5fddb67f678ed197114
Douglas Ronne <vextorspace@gmail.com>
2025-01-08 09:33:04 -0800

    "added file watch for TCR changes"

--- src/commands/watch_tcr_command.rs
+++ src/commands/watch_tcr_command.rs
@@ -1,6 +1,9 @@
 use crate::assistant::assistant::Assistant;
 use crate::commands::command::Command;
 use std::path::PathBuf;
+use crate::assistant::assistant_factory::AssistantFactory;
+use notify::Watcher;
+
 pub(crate) struct WatchTcrCommand {
 }
 
@@ -11,8 +14,40 @@ impl WatchTcrCommand {
 }
 
 impl Command for WatchTcrCommand {
-    fn execute(&self, assistant: Box<&dyn Assistant>, path: PathBuf) -> anyhow::Result<()> {
-        todo!()
+    fn execute(&self, _assistant: Box<&dyn Assistant>, path: PathBuf) -> anyhow::Result<()> {
+        let path_clone = path.clone();
+
+        let mut watcher = notify::recommended_watcher(move |res: Result<notify::Event, notify::Error>| {
+            match res {
+                Ok(event) => {
+                    println!("EVENT: {event:?}");
+
+                    match event.kind {
+                        notify::EventKind::Modify(_) |
+                        notify::EventKind::Create(_) |
+                        notify::EventKind::Remove(_) => {
+                            let result = AssistantFactory::with_ai_commit()
+                                .tcr(path_clone.clone());
+                            if let Err(e) = result {
+                                eprintln!("Error running TCR: {e}");
+                            }
+                        },
+                        _ => { },
+                    }
+                },
+                Err(e) => {
+                    eprintln!("Error: {e}");
+                }
+            }
+        })?;
+
+        watcher.watch(&*path, notify::RecursiveMode::Recursive)?;
+
+        println!("Press Ctrl-C to stop Watching for changes in: {:?}", path);
+        loop {
+            std::thread::sleep(std::time::Duration::from_secs(1));
+        }
+
     }
 
     fn get_label(&self) -> &str {
